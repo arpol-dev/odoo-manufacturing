b2defa602375f6506899b6b293cabed9ee4f1e35
ArPol <armand@arpol.fr>
2024-03-19 22:28:36 +0100

    Initial commit

---
+++ mrp_simulation/__manifest__.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Simulation de production",
+    "version": "14.0.0.0.0",
+    "category": "Manufacturing",
+    "author": "ArPol-dev",
+    "license": "AGPL-3",
+    "website": "https://github.com/arpol-dev",
+    "depends": ["Manufacturing", "sale"],
+    "installable": True,
+}
